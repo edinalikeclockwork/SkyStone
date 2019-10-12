fecab0039f4c7819de6eb9c6ed69121d36cb156e
SavCam18 <43095327+SavCam18@users.noreply.github.com>
2019-10-12 11:10:51 -0500

    Update ServoPickupTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoPickupTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoPickupTest.java
@@ -1,24 +1,17 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Hardware;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
-import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
-import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
-
-@Autonomous(name = "ServoTest")
+@Autonomous(name = "ServoPickupTest")
 
 // @Disabled
 
-public class ServoTest extends LinearOpMode {
+public class ServoPickupTest extends LinearOpMode {
 
     /* Declare OpMode members. */
 
@@ -26,28 +19,9 @@ public class ServoTest extends LinearOpMode {
 
     private ElapsedTime runtime = new ElapsedTime();
 
-    Orientation             lastAngles = new Orientation();
-    double globalAngle, power = .60, correction;
-
     // Create static variables
-    static final double COUNTS_PER_MOTOR_REV = 1440;    // eg: TETRIX Motor Encoder
-    static final double DRIVE_GEAR_REDUCTION = 0.8;     // This is < 1.0 if geared UP
-    static final double WHEEL_DIAMETER_INCHES = 4.0;     // For figuring circumference
-    static final double DRIVE_SPEED = 0.5;
-    static final double TURN_SPEED = 0.3;
-    static final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
-                                          (WHEEL_DIAMETER_INCHES * 3.1415);
-    static final double deployServoPosition = 0.7;
-    static final double openLiftLockServoPosition = 0.9;
-    static final double resetServoPosition = 0;
-    static final double liftPower = 0.4;
-    static boolean touchState = true;
-    static int down = -1;   // Reverses direction of motors to lower lift
+   
     static boolean testServoState = true;
-
-    // 09-14-2019: Servo Testing
-    static final double testServoOpen = 0.8;
-    static final double testServoClosed = 1.0;
 
     @Override
     public void runOpMode() throws InterruptedException {
