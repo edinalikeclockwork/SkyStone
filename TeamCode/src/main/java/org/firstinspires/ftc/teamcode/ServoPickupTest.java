fc48e38071e422e39551fd0d85e3dcd3c28421a3
SavCam18 <43095327+SavCam18@users.noreply.github.com>
2019-10-12 11:28:54 -0500

    Cleaned up code
    Cleaned up code and deleted unnecessary code, same applies to the nearest prior unnamed update/change to this code. Also the final two lines will have to be changed so that they can function properly.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoPickupTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoPickupTest.java
@@ -36,25 +36,7 @@ public class ServoPickupTest extends LinearOpMode {
 
         robot.init(hardwareMap);
 
-        // Send telemetry message to indicate successful Encoder reset
-        telemetry.addData("Version", "0.1");
-        //telemetry.addData("Path0",  "Starting at %7d :%7d",
-                //robot.leftDrive.getCurrentPosition(),
-                //robot.rightDrive.getCurrentPosition());
-        telemetry.update();
-
-        // TODO: Init gyro - should set to zero (? validate this)
-        telemetry.addData("Mode", "calibrating...");
-        telemetry.update();
-
-        // make sure the imu gyro is calibrated before continuing.
-        while (!isStopRequested() && !robot.imu.isGyroCalibrated()) {
-            sleep(50);
-            idle();
-        }
-
         telemetry.addData("Mode", "waiting for start");
-        telemetry.addData("imu calib status", robot.imu.getCalibrationStatus().toString());
         telemetry.update();
 
         // Wait for the game to start (driver presses PLAY)
@@ -63,14 +45,9 @@ public class ServoPickupTest extends LinearOpMode {
         // Start autonomous actions here
         // Path: Launch from Crater side
 
-        // 09-14-2019: Servo Testing
-        robot.testServo.setPosition(testServoOpen);
-        sleep(3000);
-        robot.testServo.setPosition(testServoClosed);
-        sleep(3000);
-        // robot.testServo.setPosition(testServoOpen);
-
-        sleep(50000);
+        // 10-12-2019: Servo Testing
+        robot.servoLeft.setPosition(testServoOpen);
+        robot.servoRight.setPosition(testServoOpen);
     }
 
 }
