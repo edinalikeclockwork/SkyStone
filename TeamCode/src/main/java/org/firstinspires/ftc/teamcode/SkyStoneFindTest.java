2662a6cacf53d410cb27b216b9fdf4329979d997
Willmuth <55771101+Willmuth@users.noreply.github.com>
2019-12-14 13:03:44 -0600

    Added import to Vuforia class.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkyStoneFindTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkyStoneFindTest.java
@@ -16,6 +16,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.teamcode.VuforiaSkyStoneNavigation;
 
 @Autonomous(name = "Auto - Loading Side")
 
